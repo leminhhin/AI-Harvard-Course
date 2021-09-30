6582c492d8c702f9023f7bec337fa02375b384e3
leminhhin <minh.hin.le@gmail.com>
2021-09-30 16:25:55 +0700

    update

--- Project 0 Search/tictactoe/tictactoe.py
+++ Project 0 Search/tictactoe/tictactoe.py
@@ -3,7 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
-
+import copy 
 X = "X"
 O = "O"
 EMPTY = None
@@ -28,8 +28,8 @@ def player(board):
             if cell is not EMPTY:
                 turn += 1
     if turn % 2 == 0:
-        return 'X'
-    return 'O'
+        return X
+    return O
 
 def actions(board):
     """
@@ -46,7 +46,9 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    new_board = board
+    new_board = copy.deepcopy(board)
+    i = action[0]
+    j = action[1]
     new_board[i][j] = player(board)
     return new_board
 
@@ -55,25 +57,92 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    win = None
 
+
+    for i in range(len(board)):
+        if board[i][0] == board[i][1] == board[i][2]:
+            win = board[i][0]
+        if board[0][i] == board[1][i] == board[2][i]:
+            win = board[0][i]
+
+    if board[0][0] == board[1][1] == board[2][2]:
+        win = board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        win = board[0][2]
+
+
+
+    return win
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
-
+    if winner(board) is not None:
+        return True
+    for row in board:
+        for cell in row:
+            if cell is EMPTY:
+                return False
+    return True
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
 
+def max_value(board):
+    if terminal(board):
+        return (utility(board))
+    else:
+        v = -math.inf
+        for action in actions(board):
+            v = max(v,min_value(result(board,action)))
+        return v
+
+def min_value(board):
+    if terminal(board):
+        return utility(board)
+    else:
+        v = math.inf
+        for action in actions(board):
+            v = min(v,max_value(result(board,action)))
+        return v
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    optimal_move = None
+    current_player = player(board)
+    
+    if current_player == X:
+        if board == initial_state():
+            return (1,1)
+        best_score = -math.inf
+        for action in actions(board):
+            eval_score = min_value(result(board,action))
+            if eval_score > best_score:
+                optimal_move = action
+                best_score = eval_score 
+    else:
+        best_score = math.inf
+        for action in actions(board):
+            eval_score = max_value(result(board,action))
+            if eval_score < best_score:
+                optimal_move = action
+                best_score = eval_score 
+
+    return optimal_move
+   
